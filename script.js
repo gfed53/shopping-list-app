312d2a02f310e8b06352f195f85adeaa15aac018
gfed53 <gfederico7153@gmail.com>
2015-10-30 14:21:09 -0400

    Added X show/hide on mouseenter/leave

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 $(document).ready(function() {
-	$('.remove').hide();
-
-	$('.remove-box').on('hover', function() {
-		console.log('remove');
-		$('remove').show();
-	});
+	var itemCount = 0
 	$('.enter-button').on('click', function() {
 		var shoppingItem = $('.enter-item-box').val()
 		if (shoppingItem) {
+
 			$('.shopping-list ul:first').append('<li> <img class="checkoff" src="images/check.png">'+ shoppingItem + '<div class="remove-box"><img class="remove" src="images/x.png"></div> </li>');
+			$('.remove').hide();
 		}
 	});
 
-	// $('.enter-item-box').val().append
+	$('.shopping-list ul').on('mouseenter', '.remove-box', function() {
+		$(this).find('.remove').show();
+
+	});
+
+	$('.shopping-list ul').on('mouseleave', '.remove-box', function() {
+		$(this).find('.remove').hide();
+	});
+
+
+
+
 
 });
