c1d9e56673dafdcc3d4c09e11c036d05051ec702
gfed53 <gfederico7153@gmail.com>
2015-11-01 09:27:43 -0500

    Added animations in check icon, adding and removing single items

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
 	$('.enter-button').on('click', function() {
 		var shoppingItem = $('.enter-item-box').val()
 		if (shoppingItem) {
-
-			$('.shopping-list ul:first').append('<li> <img class="checkoff" src="images/check.png">'+ shoppingItem + '<div class="remove-box"><img class="remove" src="images/x.png"></div> </li>');
+			// $('.shopping-list ul:first').append('<li> <img class="checkoff" src="images/check.png">'+ shoppingItem + '<div class="remove-box"><img class="remove" src="images/x.png"></div> </li>');
+			$('<li> <img class="checkoff" src="images/check.png">'+ shoppingItem + '<div class="remove-box"><img class="remove" src="images/x.png"></div> </li>').hide().appendTo('.shopping-list ul:first').show('normal');
 			$('.remove').hide();
 			$('.enter-item-box').val("");
 		}
@@ -20,11 +20,11 @@ $(document).ready(function() {
 	});
 
 	$('.shopping-list ul').on('mouseenter', '.checkoff', function() {
-		$(this).css({'width': '1.5em', 'height': '1.2em'});
+		$(this).animate({'width': '1.5em', 'height': '1.2em'}, 500);
 	});
 
 	$('.shopping-list ul').on('mouseleave', '.checkoff', function() {
-		$(this).css({'width': '1.3em', 'height': '1em'});
+		$(this).animate({'width': '1.3em', 'height': '1em'}, 500);
 	});
 
 	$('.shopping-list ul').on('click', '.checkoff', function() {
@@ -35,7 +35,7 @@ $(document).ready(function() {
 
 	$('.shopping-list ul').on('click', '.remove-box', function(){
 		var x = $(this).parent()
-		x.remove();
+		x.fadeOut(400, function() {x.remove();});
 	});
 
 	$('.clear-all-btn').on('click', function() {
