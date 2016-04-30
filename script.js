80e47e749b36b63d7af185f11085ebd17117a7f2
gfed53 <gfederico7153@gmail.com>
2016-04-30 17:00:55 -0400

    Updated clear all button, breakpoint tweaks

--- script.js
+++ script.js
@@ -3,15 +3,6 @@ $(document).ready(function() {
 		e.preventDefault();
 		var shoppingItem = $('.enter-item-box').val()
 		if (shoppingItem) {
-			// var item = document.createElement('li');
-			// item.className = "mdl-list__item";
-			// item.innerHTML = "<span class=\'mdl-list__item-primary-content\'>"+
-			// 	 shoppingItem+
-			// 	 "</span>"+
-			// 	 "<span class='mdl-list__item-secondary-content'></span></li>";
-			// var button = document.createElement('button');
-			// button.className = "mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored btn-remove";
-			// button.innerHTML = "<i class='material-icons'>remove</i>";
 			var element = "<li class=\'mdl-list__item\'>"+
 				"<span class=\'mdl-list__item-primary-content\'>"+
 				 shoppingItem+
@@ -22,24 +13,13 @@ $(document).ready(function() {
 				 "</button></span></li>";
 
 			$(element).hide().appendTo('.shopping-list ul:first').show('normal');
-			// componentHandler.upgradeElement($('.shopping-list ul:last button'));
-			// $('.remove').hide();
 			$('.enter-item-box').val("");
-			console.log($('.shopping-list ul:last'));
 		}
 	});
 
 	$('.shopping-list ul').on('click', 'li', function() {
-		$(this).toggleClass('checked', 400);
-		
+		$(this).toggleClass('checked', 400);	
 	});
-/*
-	$('.shopping-list ul').on('click', 'li', function() {
-		$(this).animate({ 'text-decoration': 'line-through',
-	'background-color': 'Black',
-	'color' : 'White',
-	'font-weight': '100'}, 400);
-	});  Can't work without jQuery.Color plugin */
 
 	$('.shopping-list ul').on('click', '.btn-remove', function(){
 		var parent = $(this).parent().parent();
@@ -50,9 +30,4 @@ $(document).ready(function() {
 		var x = $('.shopping-list li')
 		x.fadeOut(400, function() {x.remove();});
 	});
-
-
-
-
-
 });
